d0c66a1f256bfec351fd23f3ad25af28a6b998bd
Buruk Asrat <burukasrat@gmail.com>
2024-07-16 11:33:49 +0200

    ft_print_nbr

--- C_00/ex07/ft_putnbr.c
+++ C_00/ex07/ft_putnbr.c
@@ -11,15 +11,41 @@
 /* ************************************************************************** */
 #include <unistd.h>
 
+void	print(int nb, int sign);
+
 void	ft_putnbr(int nb)
+{
+	int	sign;
+
+	sign = 0;
+	if (nb == -2147483648)
+	{
+		ft_putnbr(-214748364);
+		nb = 8;
+	}
+
+	if (nb < 0)
+	{
+		sign = -1;
+		nb *= -1;
+	}
+
+	print(nb, sign);
+}
+
+void	print(int nb, int sign)
 {
 	int	c;
 
 	if (nb == 0)
+	{
+		if (sign == -1)
+			write(1, "-", 1);
 		return ;
+	}
 	c = nb % 10;
 	nb = nb / 10;
-	ft_putnbr(nb);
+	print(nb, sign);
 	c += 48;
 	write(1, &c, 1);
 }
